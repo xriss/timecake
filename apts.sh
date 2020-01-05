062939dc77fbc4ed96acfa15ea34225c17992eb0
kriss <kriss@xixs.com>
2020-01-05 05:25:50 +0000

    .

---
+++ apts.sh
@@ -0,0 +1,7 @@
+
+sudo apt install -y build-essential
+sudo apt install -y gcc-arm-none-eabi
+sudo apt install -y gdb-multiarch
+sudo apt install -y cmake
+
+
--- src/main.c
+++ src/main.c
@@ -5,11 +5,21 @@
 
 int main(void)
 {
+	
+	nrf_gpio_cfg_output(22);
+	nrf_gpio_cfg_output(23);
+	nrf_gpio_cfg_output(14);
+	
+	
 	while(1)
 	{
-		nrf_gpio_pin_toggle(22);
-		nrf_gpio_pin_toggle(23);
-		nrf_gpio_pin_toggle(14);
+		nrf_gpio_pin_write(22,0);
+		nrf_gpio_pin_write(23,0);
+		nrf_gpio_pin_write(14,0);
+		nrf_delay_ms(500);
+		nrf_gpio_pin_write(22,1);
+		nrf_gpio_pin_write(23,1);
+		nrf_gpio_pin_write(14,1);
 		nrf_delay_ms(500);
 	}
 
