8d979fc77346304d1480c10aabbad774b481de55
kriss <kriss@xixs.com>
2020-01-05 02:24:09 +0000

    script to fetch sdk

---
+++ nr5sdk.sh
@@ -0,0 +1,8 @@
+
+rm -rf nr5sdk
+wget https://www.nordicsemi.com/-/media/Software-and-other-downloads/SDKs/nRF5/Binaries/nRF5SDK160098a08e2.zip
+mkdir nr5sdk
+cd nr5sdk
+unzip ../nRF5SDK160098a08e2.zip
+cd ..
+rm nRF5SDK160098a08e2.zip
