74313db9f3c519f4b73e1aa2208f264a140fb975
kriss <kriss@xixs.com>
2020-01-10 23:06:40 +0000

    check saveram is actually working ram, if we overflow this test may fail as I have only allocated 256 bytes for it at the end of ram.

--- src/sys/saveram.c
+++ src/sys/saveram.c
@@ -15,21 +15,22 @@ int saveram_setup(void)
 	{
 		saveram_format();
 	}
+		saveram_format();
 	return 1;
 }
 
 void saveram_format(void)
 {
-	memset(saveram,0,sizeof(saveram));
-	
+	memset(saveram,0xf0,sizeof(saveram));
 	for(int i=0;i<sizeof(saveram);i++)
 	{
-		if( ((char*)saveram)[i]!=0 )
+		if( ((char*)saveram)[i]!=0xf0 )
 		{
 			PRINTF("Saveram write FAIL!");
 			while(1);
 		}
 	}
+	memset(saveram,0x00,sizeof(saveram));
 	
 	saveram->magick  = SAVERAM_MAGICK;
 	saveram->version = SAVERAM_VERSION;
