b0057c8972cda1ab7b8dd9fd05912a2f8edbb763
kriss <kriss@xixs.com>
2020-01-10 19:27:41 +0000

    bad copy paste

--- raspi/swd.boot.sh
+++ raspi/swd.boot.sh
@@ -1,8 +1,8 @@
 while true; do
 # try pi1
-sudo /home/pi/openocd/src/openocd -d2 -s /home/pi/openocd/tcl -f interface/raspberrypi-native.cfg  -c " bindto 0.0.0.0 ; transport select swd " -f target/nrf52.cfg -c " init ; arm semihosting enable ; reset hal>
+sudo /home/pi/openocd/src/openocd -d2 -s /home/pi/openocd/tcl -f interface/raspberrypi-native.cfg  -c " bindto 0.0.0.0 ; transport select swd " -f target/nrf52.cfg -c " init ; arm semihosting enable ; reset halt ; nrf5 info " 2>&1 | tee -a /home/pi/swd.log
 # try pi2
-sudo /home/pi/openocd/src/openocd -d2 -s /home/pi/openocd/tcl -f interface/raspberrypi2-native.cfg -c " bindto 0.0.0.0 ; transport select swd " -f target/nrf52.cfg -c " init ; arm semihosting enable ; reset hal>
+sudo /home/pi/openocd/src/openocd -d2 -s /home/pi/openocd/tcl -f interface/raspberrypi2-native.cfg -c " bindto 0.0.0.0 ; transport select swd " -f target/nrf52.cfg -c " init ; arm semihosting enable ; reset halt ; nrf5 info " 2>&1 | tee -a /home/pi/swd.log
 # wait to try again
 echo " Well that did not work... will try again in 10 secs. " | tee -a /home/pi/swd.log
 sleep 10
